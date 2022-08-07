02c546766cc3e69b864dfef270e5dba5920add52
Wendel Tarlley <wendeltarlley@gmail.com>
2022-08-07 15:06:03 -0300

    iniciando o projeto

---
+++ src/main/java/com/devTarlley/aluraChalenge/AluraChalengeApplication.java
@@ -0,0 +1,13 @@
+package com.devTarlley.aluraChalenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChalengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChalengeApplication.class, args);
+	}
+
+}
