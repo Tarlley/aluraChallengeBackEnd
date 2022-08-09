4fbeff73896a4978dce233229774e0db00cf0bbf
Wendel Tarlley <wendeltarlley@gmail.com>
2022-08-08 22:55:45 -0300

    Adicionado categoriazação das despesas

--- src/main/java/com/devtarlley/controlefinanceiro/model/Despesa.java
+++ src/main/java/com/devtarlley/controlefinanceiro/model/Despesa.java
@@ -1,12 +1,13 @@
 package com.devtarlley.controlefinanceiro.model;
 
+import com.devtarlley.controlefinanceiro.enums.Categorias;
 import lombok.*;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
-import java.time.Instant;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -15,18 +16,24 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @Table(name = "despesas")
 public class Despesa {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false)
     private Integer id;
 
     @Column(name = "DESCRICAO", nullable = false)
+    @NotBlank
     private String descricao;
 
     @Column(name = "VALOR", nullable = false)
+    @NotNull
     private Double valor;
 
     @Column(name = "DATA", nullable = false)
+    @NotNull
     private LocalDate data;
 
+    @Column(name = "CATEGORIA")
+    private Categorias categoria;
 }
