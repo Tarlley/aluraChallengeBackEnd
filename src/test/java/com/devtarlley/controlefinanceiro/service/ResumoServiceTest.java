acecdeda80f86992c867d7f2dec3c8d229319fff
Wendel Tarlley <wendeltarlley@gmail.com>
2022-08-10 23:09:54 -0300

    Melhorias na lógica de retorno da API

---
+++ src/test/java/com/devtarlley/controlefinanceiro/service/ResumoServiceTest.java
@@ -0,0 +1,13 @@
+package com.devtarlley.controlefinanceiro.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ResumoServiceTest {
+
+    @Test
+    public void deveRetornarValorPositivo(){
+
+        ResumoService resumoService = new ResumoService();
+
+    }
+}
